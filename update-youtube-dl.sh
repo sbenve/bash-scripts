93924c60d0f2c0cdde345b5e6e5a67b44bbfb323
sbenve <44841610+sbenve@users.noreply.github.com>
2021-02-28 03:27:25 -0300

    Create update-youtube-dl.sh
    initial commit

---
+++ update-youtube-dl.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#de: https://ytdl-org.github.io/youtube-dl/download.html
+wget https://yt-dl.org/downloads/latest/youtube-dl -O /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
